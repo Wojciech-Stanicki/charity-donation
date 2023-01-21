c327367c97c217d2a5a8c80b253523b22df18774
Wojciech Stanicki <stanickiw@gmail.com>
2023-01-22 00:58:12 +0100

    added Institution model

--- charity_donation/giveaway/models.py
+++ charity_donation/giveaway/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
+INSTITUTION_TYPES = (
+    (0, "fundacja"),
+    (1, "organizacja pozarządowa"),
+    (2, "zbiórka lokalna"),
+)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    type = models.IntegerField(choices=INSTITUTION_TYPES, default=0)
+    categories = models.ManyToManyField(Category)
