6fa7595c9f3a146ea93a634f52fc81445d92505f
Wojciech Stanicki <stanickiw@gmail.com>
2023-01-22 01:59:46 +0100

    slight changes in models, mainly verbose names, registered models in admin

--- charity_donation/giveaway/models.py
+++ charity_donation/giveaway/models.py
@@ -9,25 +9,45 @@ INSTITUTION_TYPES = (
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name="nazwa")
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Kategoria"
+        verbose_name_plural = "Kategorie"
 
 
 class Institution(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    type = models.IntegerField(choices=INSTITUTION_TYPES, default=0)
-    categories = models.ManyToManyField(Category)
+    name = models.CharField(max_length=100, verbose_name="nazwa")
+    description = models.TextField(verbose_name="opis")
+    type = models.IntegerField(choices=INSTITUTION_TYPES, default=0, verbose_name="typ")
+    categories = models.ManyToManyField(Category, verbose_name="kategorie")
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Instytucja"
+        verbose_name_plural = "Instytucje"
 
 
 class Donation(models.Model):
-    quantity = models.IntegerField()
-    categories = models.ManyToManyField(Category)
-    institution = models.ForeignKey(Institution, on_delete=models.SET_NULL, null=True)
-    address = models.CharField(max_length=80)
-    phone_number = models.CharField(max_length=15)
-    zip_code = models.CharField(max_length=16)
-    pick_up_date = models.DateField()
-    pick_up_time = models.TimeField()
-    pick_up_comment = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None)
+    quantity = models.IntegerField(verbose_name="ilość worków")
+    categories = models.ManyToManyField(Category, verbose_name="kategorie")
+    institution = models.ForeignKey(Institution, on_delete=models.SET_NULL, null=True, verbose_name="instytucja")
+    address = models.CharField(max_length=80, verbose_name="adres")
+    phone_number = models.CharField(max_length=15, verbose_name="numer telefonu")
+    zip_code = models.CharField(max_length=16, verbose_name="kod pocztowy")
+    pick_up_date = models.DateField(verbose_name="data odbioru")
+    pick_up_time = models.TimeField(verbose_name="czas odbioru")
+    pick_up_comment = models.TextField(verbose_name="komentarz")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None, verbose_name="użytkownik")
 
+    def __str__(self):
+        return f'dotacja {self.user.name}: {self.quantity} worków dla {self.institution.name}'
+
+    class Meta:
+        verbose_name = "Dotacja"
+        verbose_name_plural = "Dotacje"
