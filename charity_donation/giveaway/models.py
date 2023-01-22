94e72f63974465638d7316147b8bb7e8641d78d2
Wojciech Stanicki <stanickiw@gmail.com>
2023-01-22 01:30:04 +0100

    added Donation model

--- charity_donation/giveaway/models.py
+++ charity_donation/giveaway/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 INSTITUTION_TYPES = (
     (0, "fundacja"),
@@ -12,7 +13,21 @@ class Category(models.Model):
 
 
 class Institution(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     description = models.TextField()
     type = models.IntegerField(choices=INSTITUTION_TYPES, default=0)
     categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=80)
+    phone_number = models.CharField(max_length=15)
+    zip_code = models.CharField(max_length=16)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None)
+
