ec7dcd8980d12efbf7ea6ffe4329822a33a8eebe
Wojciech Stanicki <stanickiw@gmail.com>
2023-01-25 23:00:26 +0100

    added the functionality of dynamic display of institutions on landing page

--- charity_donation/giveaway/views.py
+++ charity_donation/giveaway/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 from django.db.models import Sum, Count
-from .models import Donation
+from .models import Donation, Institution
 
 
 class LandingPage(View):
     def get(self, request):
         donated_bags = Donation.objects.aggregate(sum=Sum('quantity'))
         supported_institutions = Donation.objects.aggregate(count=Count('institution', distinct=True))
+        foundations = Institution.objects.filter(type=0)
+        non_governmental_organizations = Institution.objects.filter(type=1)
+        community_collections = Institution.objects.filter(type=2)
         ctx = {
             'donated_bags': donated_bags,
             'supported_institutions': supported_institutions,
+            'foundations': foundations,
+            'non_governmental_organizations': non_governmental_organizations,
+            'community_collections': community_collections,
         }
         return render(request, "giveaway/index.html", ctx)
 
