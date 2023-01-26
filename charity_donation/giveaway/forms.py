71285e8400f53e1b02de0b9570702524e1ba8b83
Wojciech Stanicki <stanickiw@gmail.com>
2023-01-26 23:34:30 +0100

    added ability to register new users

---
+++ charity_donation/giveaway/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class UserRegisterForm(forms.Form):
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Imię'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Nazwisko'}))
+    email = forms.EmailField(max_length=100, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}))
+    password_repeat = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}))
--- charity_donation/giveaway/views.py
+++ charity_donation/giveaway/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.db.models import Sum, Count
+from django.contrib.auth.models import User
 from .models import Donation, Institution
+from .forms import UserRegisterForm
 
 
 class LandingPage(View):
@@ -32,8 +34,30 @@ class Login(View):
 
 
 class Register(View):
+    template_name = "giveaway/register.html"
+
     def get(self, request):
-        return render(request, "giveaway/register.html")
+        form = UserRegisterForm()
+        ctx = {
+            'user_register_form': form,
+        }
+        return render(request, self.template_name, ctx)
+
+    def post(self, request):
+        form = UserRegisterForm(request.POST)
+        ctx = {
+            'user_register_form': form
+        }
+        if form.is_valid():
+            User.objects.create_user(
+                username=form.cleaned_data.get('email'),
+                email=form.cleaned_data.get('email'),
+                password=form.cleaned_data.get('password'),
+                first_name=form.cleaned_data.get('first_name'),
+                last_name=form.cleaned_data.get('last_name'),
+            )
+            return redirect("login")
+        return render(request, self.template_name, ctx)
 
 
 class FormConfimation(View):
